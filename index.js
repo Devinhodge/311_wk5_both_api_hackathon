f7ce3b4603ddc5078d616be9108c541aee88bab1
Devinhodge <d.hodge8891@yahoo.com>
2020-09-23 19:36:56 -0500

    initializing

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const mysql = require('mysql');
+const app = express();
+const bodyParser = require('body-parser');
+const port = process.env.PORT || 3000
+
+
+app.listen('3000', () => {
+  console.log ('Server started on port 3000');
+});
