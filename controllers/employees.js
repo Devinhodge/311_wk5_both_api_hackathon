07890b2d8ad07e5de0808bfd94e71e61b5bf53f8
Devinhodge <d.hodge8891@yahoo.com>
2020-09-24 10:59:13 -0500

    created get employees controller function

--- controllers/employees.js
+++ controllers/employees.js
@@ -1,2 +1,12 @@
 const mysql = require('mysql')
 const pool = require('../sql/connections')
+
+const getEmployees = (req, res) => {
+  res.send('getting employees');
+}
+
+
+
+
+
+module.exports = { getEmployees}
