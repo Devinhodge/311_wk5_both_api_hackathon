649253946f27aeb960f2b60e9a4a96315d4404a7
Devinhodge <d.hodge8891@yahoo.com>
2020-09-26 11:47:38 -0500

    test

--- controllers/employees.js
+++ controllers/employees.js
@@ -24,14 +24,9 @@ const getEmployeesById = (req, res) => {
   })
 }
 
-//get by first name, is working --Devin
 const getEmployeesByFirstName = (req, res) => {
-  let sql = "SELECT * FROM ?? WHERE first_name = ?";
-  sql = mysql.format(sql, ['employees', req.params.first_name]);
-  pool.query(sql, (err, rows)=>{
-    if (err) return handleSQLError(res, err)
-    return res.json(rows);
-  })
+  
+  //should go here devin
 }
 
 
