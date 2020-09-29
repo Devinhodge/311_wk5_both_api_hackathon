8a6eb2c4492e6aade0679c21e4eeeaef81a0b7c6
mpustejovsky <mpustejovsky@yahoo.com>
2020-09-28 20:22:42 -0500

    fixed

--- intro.js
+++ intro.js
@@ -5,7 +5,7 @@ Available Routes:<br><br>
 
 /employees - Get All Employees <br>
     /employees/ID - Get employee by ID<br>
-    /employees/name/first name- Get Employee<br>
+    /employees/firstname/firstname- Get Employee<br>
 /manager - Get Current Manager of each Department<br>
 /salaries - Get Min and Max Salary of every title<br>
 /employeeCount - Get employee count of each department
